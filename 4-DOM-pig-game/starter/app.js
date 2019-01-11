042620278820aceb460ab6d580a9e11d9e267a4b
David Motz <info@davidmotz.de>
2019-01-11 09:15:43 +0100

    Adds Variables, console.log dice.

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -7,4 +7,19 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
-*/
+*/ 
+
+var scores, roundScores, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6 + 1);
+
+alert("Connected!");
+
+console.log(dice);
+
+document.querySelector('#score-0').textContend = dice;
+
